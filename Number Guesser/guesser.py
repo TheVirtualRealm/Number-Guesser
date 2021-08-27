715da85d33652f3a2d1f422fbca53fe59ea59031
TheVirtualRealm <70163600+TheVirtualRealm@users.noreply.github.com>
2021-08-27 08:47:18 -0400

    Add files via upload

---
+++ Number Guesser/guesser.py
@@ -0,0 +1,17 @@
+
+import random as r
+import random as g
+
+
+randomNumber = r.randint(1, 100)
+
+print("Take Your Guess")
+while g <= r:
+
+    randomNumber2 = g.randint(1, 100)
+
+    print(randomNumber2)
+
+
+# if g == r:
+#     print("Congratulations")
